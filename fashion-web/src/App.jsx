8315c91895850f8552095656fa0d2e4e4eac5d58
Nikita-Hsieh <nikitahsieh03@gmail.com>
2024-09-15 17:46:42 +0800

    Initial commit

---
+++ fashion-web/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <div id="home">
+        <Home />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ fashion-web/src/components/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center">
+      {/* Content Section */}
+      <h1>Discovering and Defining Your Own Fashion</h1>
+      <p>
+        Each item is carefully chosen masterpiece, promising not just fashion,
+        but a reflection of your own unique style.
+      </p>
+      <button>Shop Now</button>
+    </div>
+  );
+};
+export default Home;
