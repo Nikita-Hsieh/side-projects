dedb8b02f2966e0c04c8ea9af3001ad71a219ac5
Nikita-Hsieh <nikitahsieh03@gmail.com>
2024-10-03 22:57:03 +0800

    Header

---
+++ headphones-landing/main.js
@@ -0,0 +1,27 @@
+// Show Menu
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close")
+
+// Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu")
+  })
+}
+
+// Hide
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu")
+  })
+}
+
+//Remove Mobile Menu
+const navLink = document.querySelectorAll(".nav__link")
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu")
+  menu.classList.remove("show-menu")
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction))
