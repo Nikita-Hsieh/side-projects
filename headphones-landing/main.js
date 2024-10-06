decc3f426317211ba2a9bdc897a3b6f49cc2dbd7
Nikita-Hsieh <nikitahsieh03@gmail.com>
2024-10-06 23:05:23 +0800

    Layout Update

--- headphones-landing/main.js
+++ headphones-landing/main.js
@@ -17,34 +17,52 @@ if (navClose) {
   })
 }
 
-//Remove Mobile Menu
+// Remove Mobile Menu
 const navLink = document.querySelectorAll(".nav__link")
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu")
-  menu.classList.remove("show-menu")
+  navMenu.classList.remove("show-menu") // Fix the variable from 'menu' to 'navMenu'
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction))
 
-//Change Background Header
+// Change Background Header
 function scrollHeader() {
   const header = document.getElementById("header")
 
-  if (this.scrollY >= 50) header.classList.add("scroll-header")
+  if (window.scrollY >= 50) header.classList.add("scroll-header")
+  else header.classList.remove("scroll-header")
 }
 window.addEventListener("scroll", scrollHeader)
 
-//Scoll Up
+// Scroll Up
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up")
-  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll")
+  if (window.scrollY >= 200) scrollUp.classList.add("show-scroll")
   else scrollUp.classList.remove("show-scroll")
 }
 window.addEventListener("scroll", scrollUp)
 
-//Scroll Section Active Link
+// Scroll Section Active Link
 const sections = document.querySelectorAll("section[id]")
 
 function scrollActive() {
-  const scrollY = window.pageXOffset
+  const scrollY = window.pageYOffset
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    const sectionId = current.getAttribute("id")
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link")
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link")
+    }
+  })
 }
+window.addEventListener("scroll", scrollActive)
