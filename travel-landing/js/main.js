11156af519a042613123a2da87671a60fb68406d
Nikita-Hsieh <nikitahsieh03@gmail.com>
2024-11-04 00:08:38 +0800

    Discover Swiper

--- travel-landing/js/main.js
+++ travel-landing/js/main.js
@@ -34,3 +34,16 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader)
+
+//Swiper Discover
+var swiper = new Swiper(".discover__container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 32,
+  coverflowEffect: {
+    rotate: 0,
+  },
+})
